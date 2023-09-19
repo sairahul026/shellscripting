f2df117aa00b9d01ab856ecea0df44e436a30d34
sairahul026 <142714610+sairahul026@users.noreply.github.com>
2023-09-19 04:44:37 -0700

    Update s3-lambda-function.py

--- s3-lambda-function.py
+++ s3-lambda-function.py
@@ -1,31 +1,25 @@
 import boto3
 import json
 
-def lambda_handler(event, context):
+def lambda_trigger(event, context):
     # Extract relevant information from the S3 event trigger
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
-    object_key = event['Records'][0]['s3']['object']['key']
+    S3bucket_name = event['Records'][0]['s3']['bucket']['name']
+    bucket_object = event['Records'][0]['s3']['object']['key']
 
-    # Perform desired operations with the uploaded file
-    print(f"File '{object_key}' was uploaded to bucket '{bucket_name}'")
+    # we will be uploading a file to perform desired operations
+    print(f"File '{bucket_object}' was uploaded to bucket '{s3bucket_name}'")
 
-    # Example: Send a notification via SNS
+    # Example: this file is used to send notifications using message
     sns_client = boto3.client('sns')
     topic_arn = 'arn:aws:sns:us-east-1:<account-id>:s3-lambda-sns'
+    
     sns_client.publish(
        TopicArn=topic_arn,
        Subject='S3 Object Created',
-       Message=f"File '{object_key}' was uploaded to bucket '{bucket_name}'"
+       Message=f"File '{bucket_object}' was uploaded to bucket '{s3bucket_name}'"
     )
 
-    # Example: Trigger another Lambda function
-    # lambda_client = boto3.client('lambda')
-    # target_function_name = 'my-another-lambda-function'
-    # lambda_client.invoke(
-    #    FunctionName=target_function_name,
-    #    InvocationType='Event',
-    #    Payload=json.dumps({'bucket_name': bucket_name, 'object_key': object_key})
-    # )
+   )
 
     return {
         'statusCode': 200,
